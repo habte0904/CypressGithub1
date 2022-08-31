46cfcde65dc66890cd2a252fd4095a1693c1537d
Habtamu Petros <89646714+habte0904@users.noreply.github.com>
2022-08-31 16:47:05 +0300

    new committed

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: 'yfswa',
+  projectId: 'yfswag',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
